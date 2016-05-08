fcc3884d006f12200673414e752456d8b26e9105
nazliozge <nazli.ucan@gmail.com>
2016-05-08 20:20:55 +0300

    Update login.php

--- login.php
+++ login.php
@@ -1,30 +1,22 @@
 <html>
     <?php
         include 'connection.php';
-
         echo "<br>";
         session_start();    //alınan password ya da username'i diğer sayfalara aktarmak için session. Otomatik olarak gönderiyor sanırım.
         $uName = null;
         $pWord = null;
-
         if (mysqli_connect_errno())
         {
             echo "Failed to connect to MySQL: " . mysqli_connect_error();
         }
-
         if(isset($_GET['Login']))
         {
-
             echo "<br>";
             if(!empty($_GET["userName"]) && !empty($_GET["password"]))
             {
-
                 $uName = mysqli_real_escape_string($connection, $_GET['userName']);
                 $pWord = mysqli_real_escape_string($connection, $_GET['password']);
-
-
                 $_SESSION['userName'] = $_GET['userName']; // primary key olduğu için
-
                 $query = "SELECT * FROM member WHERE userName = '$uName' and password = '$pWord' ";
                 $result = mysqli_query($connection, $query);
                 
@@ -33,10 +25,7 @@
                     echo("Error description: " . mysqli_error($connection));
                 }
                 $count = mysqli_num_rows($result);
-
-
                 if ($count == 1) {
-
                     $query = "SELECT * FROM author WHERE userName = '$uName'";
                     $resultAuthor = mysqli_query($connection, $query);
                     if(!$resultAuthor)
@@ -44,8 +33,6 @@
                         echo("Error description: " . mysqli_error($connection));
                     }
                     $countAuthor = mysqli_num_rows($resultAuthor);
-
-
                     $query = "SELECT * FROM subscriber WHERE userName = '$uName'";
                     $resultSubsc = mysqli_query($connection, $query);
                     if(!$resultSubsc)
@@ -53,8 +40,6 @@
                         echo("Error description: " . mysqli_error($connection));
                     }
                     $countSubsc = mysqli_num_rows($resultSubsc);
-
-
                     if($countAuthor==1){
                         $goToAuthHome = true;
                     }
@@ -69,15 +54,12 @@
                 }
             }
             
-
         }
-
         if(isset($_GET['Register']))
         {
             echo "string";
             $goToReg = true;
         }
-
     ?> 
 
     <script type="text/javascript">
@@ -85,18 +67,15 @@
         if(goToAuthHome){
             window.location = 'AuthHome.php';
             window.alert("dasda1");
-
+        }
     </script>
-
     <script type="text/javascript">
         var goToSubscHome = <?php Print($goToSubscHome);?>;                
         if(goToSubscHome){
             window.alert("dasda2");
             window.location = 'SubsHome.php';
         }
-
     </script>
-
     <script type="text/javascript">
         var goToReg = <?php Print($goToReg); ?>;
         if(goToReg){
